159f392548676560c47740ff08a2a268c1dd5648
atst_96 <tnkjpei04@gmail.com>
2022-09-23 20:23:33 +0900

    add: MainProject(test)

---
+++ NEUTRINO_Test/SoundAnalyzeResult.cs
@@ -0,0 +1,3 @@
+﻿namespace NEUTRINO_Test;
+
+record SoundAnalyzeResult(long FramesCount, double[] Pitches, double[] Volumes, TimingInfo[] Timings);
---
+++ NEUTRINO_Test/TimingInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEUTRINO_Test;
+
+internal record TimingInfo(long BeginTime, long EndTime, string Lyrics);
