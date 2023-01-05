5040709f2838aac3b7d6c1eaf231115419cf09ba
atst_96 <tnkjpei04+github@gmail.com>
2023-01-06 00:06:06 +0900

    remove: 重複クラスの削除

---
+++ Quark.Share/Utils/MemoryPackUtil.cs
@@ -0,0 +1,12 @@
+﻿using MemoryPack;
+
+namespace Quark.Utils;
+
+internal static class MemoryPackUtil
+{
+    public static T Deserialize<T>(byte[] data)
+        => MemoryPackSerializer.Deserialize<T>(data)!;
+
+    public static byte[] Serialize<T>(T data)
+        => MemoryPackSerializer.Serialize(data);
+}
