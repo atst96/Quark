1184662d2330f33631c9f0d0985dacf2e300f07a
atst_96 <tnkjpei04+github@gmail.com>
2023-01-06 00:10:50 +0900

    add: データ読み書き用に不足していたメソッドを追加

--- Quark.Share/Utils/MemoryPackUtil.cs
+++ Quark.Share/Utils/MemoryPackUtil.cs
@@ -1,4 +1,5 @@
-﻿using MemoryPack;
+﻿using System.IO;
+using MemoryPack;
 
 namespace Quark.Utils;
 
@@ -7,6 +8,12 @@ internal static class MemoryPackUtil
     public static T Deserialize<T>(byte[] data)
         => MemoryPackSerializer.Deserialize<T>(data)!;
 
+    public static T ReadFile<T>(string path)
+        => Deserialize<T>(File.ReadAllBytes(path));
+
     public static byte[] Serialize<T>(T data)
         => MemoryPackSerializer.Serialize(data);
+
+    public static void WriteFile<T>(string path, T data)
+        => File.WriteAllBytes(path, Serialize(data));
 }
