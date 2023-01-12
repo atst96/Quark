add3e832df9e779cf0882701def7825bef6a0eb5
atst_96 <tnkjpei04+github@gmail.com>
2023-01-13 00:46:07 +0900

    add: shared project

---
+++ Quark.Share/Utils/GuidUtil.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Quark.Utils;
+
+internal static class GuidUtil
+{
+    public static string GetStringGuid() => Guid.NewGuid().ToString("H");
+}
---
+++ Quark.Share/VirtualFile.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+using Quark.Utils;
+
+namespace Quark;
+
+internal class VirtualFile : IDisposable
+{
+    private static readonly string _commonGuid = GuidUtil.GetStringGuid();
+    private string _pipeName;
+    private NamedPipeServerStream _namedPipeServer;
+    private bool _disposedValue;
+
+    public VirtualFile(string? suffix)
+    {
+        this._pipeName = string.Join("/", "Quark", _commonGuid, GuidUtil.GetStringGuid() + (suffix ?? string.Empty));
+        this._namedPipeServer = new(this._pipeName, PipeDirection.InOut);
+    }
+
+    public string GetPath() => Path.Join("/", "Quark", this._pipeName);
+
+    private Task WaitConnection()
+    {
+        var server = this._namedPipeServer;
+        return !server.IsConnected
+            ? server.WaitForConnectionAsync()
+            : Task.CompletedTask;
+    }
+
+    public Task<byte[]> Read() => Task.Run(() =>
+    {
+        var server = this._namedPipeServer;
+
+        server.WaitForConnection();
+
+        byte[] data;
+        using (var ms = new MemoryStream())
+        {
+            server.CopyTo(ms);
+            data = ms.ToArray();
+        }
+
+        server.Disconnect();
+
+        return data;
+    });
+
+    public Task Write(byte[] data) => this.WaitConnection().ContinueWith(t =>
+    {
+        return this._namedPipeServer.WriteAsync(data);
+    });
+
+    public void Dispose()
+    {
+        if (!this._disposedValue)
+        {
+            this._namedPipeServer.Dispose();
+            this._disposedValue = true;
+        }
+    }
+
+    ~VirtualFile()
+    {
+        this.Dispose();
+    }
+}
