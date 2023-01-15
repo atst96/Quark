b3a637d417932b4ecf2f4f7b92d21ed4937c84c2
atst_96 <tnkjpei04+github@gmail.com>
2023-01-15 17:29:53 +0900

    fix: GUID生成に失敗する不具合の修正

--- Quark.Share/Utils/GuidUtil.cs
+++ Quark.Share/Utils/GuidUtil.cs
@@ -4,5 +4,5 @@ namespace Quark.Utils;
 
 internal static class GuidUtil
 {
-    public static string GetStringGuid() => Guid.NewGuid().ToString("H");
+    public static string GetStringGuid() => Guid.NewGuid().ToString("B");
 }
