a82a59c55298b3e2251be8c39bb4ba765b1529af
atst_96 <tnkjpei04+github@gmail.com>
2023-01-15 23:14:42 +0900

    clean: プロジェクトの整理

--- NEUTRINO_Test/TimingInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NEUTRINO_Test;
-
-internal record TimingInfo(long BeginTime, long EndTime, string Lyrics);
--- World/README.md
+++
@@ -1,3 +0,0 @@
-# World.NET
-
-World(https://github.com/mmorise/World)のコードをNEUTRINO＆.NET向けに移植＆改変したものです。
