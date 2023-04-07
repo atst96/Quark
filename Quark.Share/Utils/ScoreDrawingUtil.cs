b1f6ee72c2c2beafe4975f0b1792ca198256d781
atst_96 <tnkjpei04+github@gmail.com>
2023-04-08 02:02:49 +0900

    improve: 計算周りのコードを少し整理

---
+++ Quark.Share/Utils/ScoreDrawingUtil.cs
@@ -0,0 +1,257 @@
+﻿using System.Collections.Immutable;
+using System.Runtime.CompilerServices;
+using Quark.Drawing;
+using Quark.Models.Scores;
+
+namespace Quark.Utils;
+
+/// <summary>
+/// 譜面描画に関するUtilクラス
+/// </summary>
+public static class ScoreDrawingUtil
+{
+    /// <summary>全音符</summary>
+    private const double Whole = 4d / 1;
+    /// <summary>2分音符</summary>
+    private const double Note2th = 4d / 2;
+    /// <summary>4分音符</summary>
+    private const double Note4th = 4d / 4;
+    /// <summary>8分音符</summary>
+    private const double Note8th = 4d / 8;
+    /// <summary>16分音符</summary>
+    private const double Note16th = 4d / 16;
+    /// <summary>32分音符</summary>
+    private const double Note32th = 4d / 32;
+    /// <summary>64分音符</summary>
+    private const double Note64th = 4d / 64;
+    /// <summary>128分音符</summary>
+    private const double Note128th = 4d / 128;
+
+    private static readonly ImmutableArray<(double Coe, LineType Type)> NoteDurationListForLineType = ImmutableArray.Create(
+        // 全音符
+        (Whole, LineType.Whole),
+        // 2分音符
+        (Note2th, LineType.Note2th),
+        // 4分音符
+        (Note4th, LineType.Note4th),
+        // 8分音符
+        (Note8th, LineType.Note8th),
+        // 16分音符
+        (Note16th, LineType.Note16th),
+        // 32分音符
+        (Note32th, LineType.Note32th),
+        // 64分音符
+        (Note64th, LineType.Note64th),
+        // 128分音符
+        (Note128th, LineType.Note128th),
+
+        // 三連符系
+        // 2分三連符
+        (ToTriplet(Note2th), LineType.Note2thTriplet),
+        // 4分三連符
+        (ToTriplet(Note4th), LineType.Note4thTriplet),
+        // 8分三連符
+        (ToTriplet(Note8th), LineType.Note8thTriplet),
+        // 16分三連符
+        (ToTriplet(Note16th), LineType.Note16thTriplet),
+        // 32分三連符
+        (ToTriplet(Note32th), LineType.Note32thTriplet),
+        // 64分三連符
+        (ToTriplet(Note64th), LineType.Note64thTriplet),
+
+        // 付点系
+        // 2分付点
+        (ToDotted(Note2th), LineType.Note2thDotted),
+        // 4分付点
+        (ToDotted(Note4th), LineType.Note4thDotted),
+        // 8分付点
+        (ToDotted(Note8th), LineType.Note8thDotted),
+        // 16分付点
+        (ToDotted(Note16th), LineType.Note16thDotted),
+        // 32分付点
+        (ToDotted(Note32th), LineType.Note32thDotted),
+        // 64分付点
+        (ToDotted(Note64th), LineType.Note64thDotted)
+    );
+
+    /// <summary>音符の尺を付点付きにする</summary>
+    /// <param name="duration">元の音符の尺</param>
+    /// <returns>付点音符の尺</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static double ToDotted(double duration) => duration * 1.5d;
+
+    /// <summary>音符の尺を三連符にする</summary>
+    /// <param name="duration">元の音符の尺</param>
+    /// <returns>三連符の尺</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static double ToTriplet(double duration) => duration * 4.0d / 3.0d;
+
+    /// <summary>罫線の種別を計算する</summary>
+    /// <param name="duration">元の音符の尺</param>
+    /// <param name="count">小節内の繰り返し数</param>
+    /// <returns>罫線の種別</returns>
+    private static LineType? GetLineType(double duration, int count)
+    {
+        if (count == 0) // 最初は小節
+            return LineType.Measure;
+
+        // 小節上の位置
+        // ※この処理内ではテンポ変更を考慮しなくてよい
+        double positionInMeasure = duration * count;
+
+        foreach (var noteDuration in NoteDurationListForLineType)
+        {
+            // 罫線の種別分繰り返す
+
+            // 小節の先頭から指定位置までの尺が罫線種別の尺でぴったり割り切れるのなら、その罫線種別を返す
+            double div = positionInMeasure / noteDuration.Coe;
+            if (div == Math.Floor(div))
+            {
+                return noteDuration.Type;
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// 罫線の位置を計算する
+    /// </summary>
+    /// <param name="score">譜面情報</param>
+    /// <param name="beginTime">開始時間</param>
+    /// <param name="endTime">終了時間</param>
+    /// <param name="lineType">罫線種別</param>
+    /// <returns></returns>
+    public static VerticalLineInfo[] GetVerticalLines(PartScore score, decimal beginTime, decimal endTime, LineType lineType)
+    {
+        var list = new LinkedList<VerticalLineInfo>();
+
+        var tempos = score.Tempos;
+
+        var measureTimes = ScoreUtil.GetMeasureTimes(beginTime, endTime, score);
+
+        var currentTempoNode = tempos.First!;
+        while (currentTempoNode != null)
+        {
+            var nextTempoNode = currentTempoNode.Next;
+            if (nextTempoNode is not null && measureTimes[0] > nextTempoNode.Value.Time)
+            {
+                currentTempoNode = nextTempoNode;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        // 1音あたりの長さ比率を計算
+        decimal noteDurationCoe = 0xFF & (ushort)lineType;
+        if (lineType.HasFlag(LineType.Dotted))
+        {
+            // 付点音符は1.5倍する
+            noteDurationCoe /= 1.5m;
+        }
+        else if (lineType.HasFlag(LineType.Triplet))
+        {
+            // 連符は1.3333...倍する
+            noteDurationCoe = noteDurationCoe * 3.0m / 4.0m;
+        }
+
+        double lineDuration = GetLineTypeDuration(lineType);
+
+        for (int idx = 1; idx < measureTimes.Length; ++idx)
+        {
+            decimal measureBeginTime = measureTimes[idx - 1];
+            decimal measureDuration = measureTimes[idx - 0] - measureBeginTime;
+
+            var nextTempoNode = currentTempoNode!.Next;
+            if (nextTempoNode is not null && measureBeginTime > nextTempoNode.Value.Time)
+            {
+                currentTempoNode = nextTempoNode;
+            }
+
+            // 計算中の小節の長さ(拍の長さを足し合わせたもの)
+            decimal calculateBeatDuration = 0.0m;
+
+            // 音符1つあたりの尺(ms)
+            decimal currentTempoNoteDuration = 60 * 1000 * (4.0m / noteDurationCoe);
+
+            // 線数
+            int count = 0;
+
+            while (calculateBeatDuration < measureDuration)
+            {
+                //  1小節の末尾まで繰り返す
+
+                // 拍の長さ
+                decimal beatDuration = 0.0m;
+                decimal currentPerTime = measureBeginTime + calculateBeatDuration;
+                decimal currentTime = currentPerTime;
+
+                decimal beatProgress = 0.0m;
+                while (beatProgress <= 1.0m)
+                {
+                    // 泊の末尾まで繰り返す
+                    // 同じ拍内で複数回のテンポ変更がある場合を考慮している
+
+                    decimal currentTempo = (decimal)currentTempoNode.Value.Tempo;
+
+                    // 解析中のハンクの長さの残りの部分を予測する
+                    decimal candidateRemaining = currentTempoNoteDuration * (1.0m - beatProgress) / currentTempo;
+
+                    if (nextTempoNode is null || (currentTime + candidateRemaining) <= nextTempoNode.Value.Time)
+                    {
+                        // 以下のいずれかの条件
+                        // ・次のテンポ変更がない
+                        // ・予測した泊の終わり時間が次のテンポ変更時間以前
+
+                        // 拍の長さの解析を終わる
+                        beatDuration += candidateRemaining;
+                        beatProgress = 1.0m;
+                        break;
+                    }
+
+                    decimal nextTime = nextTempoNode.Value.Time;
+
+                    // 現在時間から変更された位置までを泊の長さに含める
+                    decimal changeDuration = nextTime - currentTime;
+                    beatDuration += changeDuration;
+
+                    // 変更された区間が現在の泊の始まりから何%にあたるかを計算し、解析率に加える
+                    decimal changeCoe = changeDuration / (currentTempoNoteDuration / currentTempo);
+                    beatProgress += changeCoe;
+
+                    currentTime = nextTime;
+                    currentTempoNode = nextTempoNode;
+                    nextTempoNode = nextTempoNode.Next;
+                }
+
+                //decimal measureEndTime = currentPerTime + beatDuration;
+
+                list.AddLast(new VerticalLineInfo(currentPerTime, GetLineType(lineDuration, count) ?? lineType));
+
+                // 現在時間に泊の尺を追加
+                calculateBeatDuration += beatDuration;
+                ++count;
+            }
+        }
+
+        return list.ToArray();
+    }
+
+    /// <summary></summary>
+    /// <param name="type">音符の種別</param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double GetLineTypeDuration(LineType type)
+    {
+        double duration = 4.0d / (0xFF & (ushort)type);
+
+        if (type.HasFlag(LineType.Dotted))
+            return ToDotted(duration);
+        else if (type.HasFlag(LineType.Triplet))
+            return ToTriplet(duration);
+        else
+            return duration;
+    }
+}
---
+++ Quark.Share/Utils/ScoreUtil.cs
@@ -0,0 +1,144 @@
+﻿using Quark.Models.Scores;
+
+namespace Quark.Utils;
+
+/// <summary>
+/// 楽譜処理に関するUtilクラス
+/// </summary>
+public static class ScoreUtil
+{
+    /// <summary>
+    /// 譜面情報から描画開始・終了位置の小節位置を解析する
+    /// </summary>
+    /// <param name="beginTime">描画開始位置</param>
+    /// <param name="endTime">描画終了位置</param>
+    /// <param name="score">譜面情報</param>
+    /// <returns></returns>
+    public static decimal[] GetMeasureTimes(decimal beginTime, decimal endTime, PartScore score)
+    {
+        var measureTimeList = new LinkedList<decimal>();
+
+        var tempos = score.Tempos;
+        var timeSignatures = score.TimeSignatures;
+
+        var currentTempoNode = tempos.First!;
+        var currentTimeSignatureNode = timeSignatures.First!;
+
+        for (decimal currentMeasureTime = score.BeginMeasureTime; currentMeasureTime <= endTime;)
+        {
+            var nextTempoNode = currentTempoNode.Next;
+            if (nextTempoNode is not null && (int)nextTempoNode.Value.Time <= (int)currentMeasureTime)
+            {
+                // 次の拍変更に到達
+                currentTempoNode = nextTempoNode;
+                nextTempoNode = nextTempoNode.Next;
+            }
+
+            var nextTimeSignatureNode = currentTimeSignatureNode.Next;
+            if (nextTimeSignatureNode is not null && (int)nextTimeSignatureNode.Value.Time <= (int)currentMeasureTime)
+            {
+                // テンポ変更に到達
+                currentTimeSignatureNode = nextTimeSignatureNode;
+                currentMeasureTime = nextTimeSignatureNode.Value.Time;
+            }
+
+            var timeSignature = currentTimeSignatureNode.Value;
+
+            const decimal BeatTypeLength4th = 4m;
+
+            // 1小節分の4分音符あたりの長さ(ミリ秒)
+            // 4拍子÷拍子記号の分母×拍数
+            // 4/4拍子 → 1.0x4、 6/8拍子 → 0.5x8、 1/2拍子 → 2.0x1
+            decimal beatsPer4thMs = 60 * 1000 * BeatTypeLength4th * timeSignature.Beats / timeSignature.BeatType;
+
+            // 余分な計算の回避処置
+            if (currentMeasureTime < beginTime)
+            {
+                // 最初の小節から描画位置まで複数小節ある場合は、計算回数を減らすためにその間の計算を飛ばす
+
+                // 1小節分の長さ(ms)を予測する
+                decimal skipDurationPerMeasure = beatsPer4thMs / (decimal)currentTempoNode.Value.Tempo;
+
+                //  描画開始位置、テンポ変更、拍子変更のうち一番早いイベントの時間を探す
+                decimal earliestTime = beginTime;
+
+                if (nextTempoNode is not null && beginTime > nextTempoNode.Value.Time)
+                    earliestTime = nextTempoNode.Value.Time;
+
+                if (nextTimeSignatureNode is not null && beginTime > nextTimeSignatureNode.Value.Time)
+                    earliestTime = nextTimeSignatureNode.Value.Time;
+
+                // 一番早いイベントから計算を飛ばせる小節数が1つ以上あれば現在時間に加える
+                decimal skipCount = Math.Floor((earliestTime - currentMeasureTime) / skipDurationPerMeasure);
+                if (skipCount > 0)
+                    currentMeasureTime += skipCount * skipDurationPerMeasure;
+            }
+
+            // 現在の小節がどの程度解析できたかを示す値(0～1.0)
+            decimal measureProgress = 0.0m;
+            // 現在の譜面上の時刻(ms)
+            decimal currentTime = currentMeasureTime;
+
+            // 現在の小節の長さ
+            decimal duration = 0.0m;
+
+            while (measureProgress <= 1.0m)
+            {
+                // 小節の末尾まで繰り返す
+                // 同じ小節内で複数回のテンポ変更がある場合を考慮している
+
+                decimal currentTempo = (decimal)currentTempoNode.Value.Tempo;
+
+                // 解析中の小節の長さの残りの部分を予測する
+                decimal candidateRemaining = beatsPer4thMs * (1.0m - measureProgress) / currentTempo;
+
+                if (nextTempoNode is null || (currentTime + candidateRemaining) <= nextTempoNode.Value.Time)
+                {
+                    // 以下のいずれかの条件
+                    // ・次のテンポ変更がない
+                    // ・予測した小節の終わり時間が次のテンポ変更時間以前
+
+                    // 小節の長さの解析をおしまいにする
+                    duration += candidateRemaining;
+                    measureProgress = 1.0m;
+                    break;
+                }
+
+                decimal nextTime = nextTempoNode.Value.Time;
+
+                // 現在時間から変更された位置までを小節の長さに含める
+                decimal changedDuration = nextTime - currentTime;
+                duration += changedDuration;
+
+                // 変更された区間が現在の小節の始まりから何%にあたるかを計算し、解析率に加える
+                decimal changeCoe = changedDuration / (beatsPer4thMs / currentTempo);
+                measureProgress += changeCoe;
+
+                currentTime = nextTime;
+                currentTempoNode = nextTempoNode;
+                nextTempoNode = nextTempoNode.Next;
+            }
+
+            // 小節リスト追加する
+            decimal measureEndTime = currentMeasureTime + duration;
+            if (beginTime < measureEndTime || (beginTime <= currentMeasureTime && currentMeasureTime <= endTime))
+            {
+                // 以下のいずれかの場合
+                // ・小節の開始～終了時間が描画開始位置を跨いでいる
+                // ・現在の小節の開始時間が描画範囲内
+                measureTimeList.AddLast(currentMeasureTime);
+
+                if (endTime < measureEndTime)
+                {
+                    // 小節の終了時間が描画範囲外の場合は終了時間を追加する
+                    measureTimeList.AddLast(measureEndTime);
+                }
+            }
+
+            // 現在時間に小節の尺を追加
+            currentMeasureTime = measureEndTime;
+        }
+
+        return measureTimeList.ToArray();
+    }
+}
