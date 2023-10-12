a471bcbc156ea9cb097092abb559c2f0ee575ab6
atst_96 <tnkjpei04+github@gmail.com>
2023-10-13 00:53:12 +0900

    improve: 例外発生時にエラーダイアログを表示するように変更

--- Quark.Prototype/App.xaml.cs
+++ Quark.Prototype/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows;
 using Microsoft.Extensions.DependencyInjection;
 using Quark.DependencyInjection;
@@ -19,6 +20,38 @@ public partial class App : Application
     public static App Instance => _instance ??= (App)Current;
 
     public IServiceProvider? ServiceProvider { get; private set; }
+
+    public App() : base()
+    {
+        AppDomain.CurrentDomain.UnhandledException += this.OnUnhandledException;
+    }
+
+    /// <summary>
+    /// 例外キャッチ時
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        if (!Debugger.IsAttached)
+        {
+            // 非デバッグ時はダイアログを表示してアプリケーションが突然落ちるのを防ぐ
+
+            var exception = (Exception)e.ExceptionObject;
+
+            System.Windows.Forms.TaskDialog.ShowDialog(new()
+            {
+                Caption = "Quark",
+                Heading = "予期しないエラーでアプリケーションが終了しました",
+                Text = exception.Message,
+                Icon = System.Windows.Forms.TaskDialogIcon.Error,
+                Expander = new()
+                {
+                    Text = exception.StackTrace,
+                }
+            });
+        }
+    }
 
     /// <summary>
     /// 起動時
