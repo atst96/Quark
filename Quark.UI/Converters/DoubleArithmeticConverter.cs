b1fe44510db775e39e883daa1dd386040421ec59
atst_96 <tnkjpei04@gmail.com>
2024-03-03 12:05:53 +0900

    add: Quark.UIにテーマデータを移植

---
+++ Quark.UI/Converters/DoubleArithmeticConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Quark.Converters;
+
+public class DoubleArithmeticConverter : IValueConverter
+{
+    public ArithmeticOperation Operation { get; set; } = ArithmeticOperation.Add;
+
+    public double RightValue { get; set; }
+
+    public enum ArithmeticOperation
+    {
+        Add,
+    }
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null)
+            return value;
+
+        if (value is double d)
+            return d + this.RightValue;
+
+        throw new NotSupportedException();
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null)
+            return value;
+
+        if (value is double d)
+            return d + this.RightValue;
+
+        throw new NotSupportedException();
+    }
+}
